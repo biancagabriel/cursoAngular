b16fc89dd46d5373531ee3850d6ae8542262de2f
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-05-29 13:03:53 -0300

    componentes e estilo css

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule,RouterOutlet,NewComponent],
+  template: `
+  <!-- <router-outlet></router-outlet> -->
+  <h1>Curso de Angular</h1>
+  <div class="theme-red">
+  <app-new-component/>
+  </div>
+  `
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto-latest';
-}
+export class AppComponent {}
+
