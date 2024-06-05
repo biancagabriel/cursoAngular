3e5f6aa80d12179a142375f8729ed67c2673bb8a
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-06-05 14:54:12 -0300

    property binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
   public name = 'Bianca';
   public age=28;
-
+  public condition = this.age > 1 ? 'teste 1' : 'teste 2';
+  public isDesabled = false;
+  //  public srcValue = 
+  // 'https://devbiagabriel.wordpress.com/wp-content/uploads/2013/03/bia.jpg?w=640';
+  
   public sum(val1:number,val2:number){
     return val1 + val2;
   }
