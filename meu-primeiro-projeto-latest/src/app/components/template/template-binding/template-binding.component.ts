d9270faea1c94738e55648a950439e5b9646305c
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-06-05 16:06:20 -0300

    two-way binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
