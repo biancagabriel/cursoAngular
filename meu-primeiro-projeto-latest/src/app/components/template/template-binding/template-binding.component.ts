fe7ed5de04d256b935df564ed1e747a68d4675e8
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-06-05 15:46:37 -0300

    event binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -11,12 +11,24 @@ import { CommonModule } from '@angular/common';
 export class TemplateBindingComponent {
   public name = 'Bianca';
   public age=28;
-  public condition = this.age > 1 ? 'teste 1' : 'teste 2';
+  public condition = this.age > 1 ? 'teste ' : 'teste 2';
   public isDesabled = true;
   //  public srcValue = 
   // 'https://devbiagabriel.wordpress.com/wp-content/uploads/2013/03/bia.jpg?w=640';
   public isTextDecoration = this.age >=28 ? 'underline' : 'none';
-  public sum(val1:number,val2:number){
-    return val1 + val2;
+  public sum(){
+    return this.age++;
+  }
+  public sub(){
+    return this.age--;
+  }
+  public onKeyDown(event: Event){
+  return console.log(event);
+  } 
+  public onMouseMove(event : MouseEvent){
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY,
+    });
   }
 }
