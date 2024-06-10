39a8b0f891d945a5be79324ecfa41b0071267120
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-06-10 09:05:52 -0300

    template variables

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -4,17 +4,20 @@ import { RouterOutlet } from '@angular/router';
 //components
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,NewComponent,TemplateBindingComponent],
+  imports: [CommonModule,RouterOutlet,NewComponent,TemplateBindingComponent,TemplateVariablesComponent],
   template: `
   <!-- <router-outlet></router-outlet> -->
   <!-- <app-new-component/> -->
+  <!-- <app-template-binding/> -->
   <h1>Curso de Angular</h1>
-  <app-template-binding/>
+  <app-template-variables>
+  
   `,
 })
 export class AppComponent {}
