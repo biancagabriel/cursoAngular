012c9b2feda9907667b5ee8730af620e9c912493
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-07-01 21:38:23 -0300

    @switch case

--- meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,16 +1,17 @@
-import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule, NgIf, NgFor],
+  imports: [CommonModule, NgIf, NgFor, NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
+  public switchCondition = 'A';
 
   public itens: Array<{name: string}>= [{name: 'bianca gabriel'}];
   public loadingData$: Observable<string[]> = of([
